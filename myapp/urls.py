815ad6a09b1a06235cc566fa069084b06cdeb08b
sstieff <144176441+sstieff@users.noreply.github.com>
2024-06-02 18:03:48 +0000

    Added Class and Function views, bootswatch for all pages, and images for all pages

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("", views.home, name="home"),
-  path("about",views.about, name="about")
+  path("", views.HomePage.as_view(), name="home"),
+  path("about/",views.AboutPage.as_view(), name="about"),
+  path('function/', views.function_view, name='function_view'),
+  path('class/', views.ClassView.as_view(), name='class_view'),
 ]
